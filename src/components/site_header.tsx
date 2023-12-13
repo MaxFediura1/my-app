a1a500729888f52a83d1671a140a38fa0778ef2f
MaxFediura1 <maxfediura@gmail.com>
2023-12-13 23:21:53 +0200

    a to link

--- src/components/site_header.tsx
+++ src/components/site_header.tsx
@@ -29,13 +29,13 @@ export function SiteHeader() {
     <header className="bg-white h-24 fixed z-10 w-screen"   >
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
-          <a href="/" className="-m-1.5 p-1.5">
+          <Link href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
             <Image className="h-12 w-auto" 
             src={logo}
             alt="" 
             />
-          </a>
+          </Link>
         </div>
         <div className="flex lg:hidden">
           <button
@@ -82,32 +82,32 @@ export function SiteHeader() {
             </Transition>
           </Popover>
 
-          <a href="about_us" className="text-sm font-semibold leading-6 text-gray-900">
+          <Link href="about_us" className="text-sm font-semibold leading-6 text-gray-900">
           About us
-          </a>
-          <a href="gallery" className="text-sm font-semibold leading-6 text-gray-900">
+          </Link>
+          <Link href="gallery" className="text-sm font-semibold leading-6 text-gray-900">
           Gallery
-          </a>
-          <a href="useful_information" className="text-sm font-semibold leading-6 text-gray-900">
+          </Link>
+          <Link href="useful_information" className="text-sm font-semibold leading-6 text-gray-900">
           Useful information
-          </a>
-          <a href="contacts" className="text-sm font-semibold leading-6 text-gray-900">
+          </Link>
+          <Link href="contacts" className="text-sm font-semibold leading-6 text-gray-900">
           Contacts
-          </a>
+          </Link>
         </Popover.Group>
       </nav>
       <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="/" className="-m-1.5 p-1.5">
+            <Link href="/" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
               <Image 
                 className="h-8 w-auto"
                 src={logo}
                 alt=""
               />
-            </a>
+            </Link>
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-gray-700"
@@ -145,30 +145,30 @@ export function SiteHeader() {
                     </>
                   )}
                 </Disclosure>
-                <a
+                <Link
                   href="about_us"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   About us
-                </a>
-                <a
+                </Link>
+                <Link
                   href="gallery"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   Gallery
-                </a>
-                <a
+                </Link>
+                <Link
                   href="useful_information"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   Useful information
-                </a>
-                <a
+                </Link>
+                <Link
                   href="contacts"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   Contacts
-                </a>
+                </Link>
               </div>
             </div>
           </div>
